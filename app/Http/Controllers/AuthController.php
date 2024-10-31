29a77d6b66685da19ce5c67a5d58a4e5e6180d7d
KarimovKozimjon <kozimjonk224@gmail.com>
2024-10-31 15:30:36 -0700

    authning loin qismini tugatdim va requestdan kelib chiqqan ko'pdan ko'p muammolarni yechdim

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
@@ -20,17 +22,33 @@ class AuthController extends Controller
         $user->username = $request->username;
         $user->email = $request->email;
         $user->password = bcrypt($request->password);
-    
+
         // Avatarni saqlash
         if ($request->hasFile('avatar')) {
             $path = $request->file('avatar')->store('avatars', 'public'); // 'public' diskda saqlash
-            $user->avatar = $path; 
+            $user->avatar = $path;
         }
-    
+
         $user->save();
-    
+
         Auth::login($user);
-    
+
         return redirect()->intended('/');
+    }
+    public function loginForm()
+    {
+        return view("auth.login");
+    }
+    public function login(LoginRequest $request)
+    {
+        $credentials = request()->only("email", "password");
+        $user = User::where("email", $request->email)->first();
+        if (!Hash::check($request->password, $user->password)) {
+            return "error";
+        }
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect("/profile");
+        }
     }
 }

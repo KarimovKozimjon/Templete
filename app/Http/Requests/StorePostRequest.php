7f71b0c97458c4b241be94c073c71ac8cc2fa0a8
KarimovKozimjon <kozimjonk224@gmail.com>
2024-10-31 15:54:45 -0700

    requestdan foydalandim va bunda title desc va imagelarni filterladim

---
+++ app/Http/Requests/StorePostRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true; // Allow all users to make this request
+    }
+
+    public function rules()
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'image' => 'required|image|mimes:jpg,jpeg,png,gif|max:2048',
+        ];
+    }
+}
